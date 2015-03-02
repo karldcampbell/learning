613f82755926bd206fd4f517bdb6cabfc52973a3
Karl D Campbell <karl.d.campbell@gmail.com>
2015-03-01 22:08:15 -0800

    doing ch16

---
+++ scala_learning/src/ch16.scala
@@ -0,0 +1,31 @@
+
+
+object ch16 extends App{
+ 
+  def msort[T](less: (T, T) => Boolean)
+    (xs: List[T]): List[T] = {  
+  
+    def merge(xs: List[T], ys: List[T]): List[T] = {
+      (xs, ys) match {
+        case (Nil, _) => ys
+        case (_, Nil) => xs
+        case(x :: xs1, y :: ys1) =>
+          if(less(x,y)) x :: merge(xs1, ys)
+          else y :: merge(xs, ys1)
+      }
+    }
+    
+    val n = xs.length
+    if(n == 0) xs
+    else {
+      val (ys, zs) = xs splitAt n
+      merge(msort(less)(ys), msort(less)(xs))
+    }
+  }
+  val f = (a: Int, b: Int) => a < b
+  
+  val l = List(1,2,6,9,3,6,2,23,77,99,12,3,3555,90)
+  
+ println(msort(f)(List(2,1)))
+  
+}
